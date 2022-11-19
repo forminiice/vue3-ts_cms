d341d3e51fcb0547b9aba47427acaf5817aff476
Hz <forminiice@qq.com>
2022-11-19 21:16:34 +0800

    feat: slidbar add

---
+++ src/components/nav-header/src/nav-header.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="nav-header">
+    <el-icon class="fold-menu" @click="handleFoldClick">
+      <Expand v-if="isFold" />
+      <Fold v-else />
+    </el-icon>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+export default defineComponent({
+  emits: ['foldChange'],
+  setup(props, { emit }) {
+    const isFold = ref(false)
+
+    const handleFoldClick = () => {
+      isFold.value = !isFold.value
+      emit('foldChange', isFold.value)
+    }
+
+    return {
+      isFold,
+      handleFoldClick
+    }
+  }
+})
+</script>
+
+<style scoped lang="less">
+.nav-header {
+  display: flex;
+  width: 100%;
+
+  .fold-menu {
+    font-size: 30px;
+    cursor: pointer;
+  }
+
+  .content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex: 1;
+    padding: 0 20px;
+  }
+}
+</style>
--- src/store/types.ts
+++ src/store/types.ts
@@ -1,4 +1,12 @@
+import { ILoginState } from './login/types'
+
 export interface IRootState {
   name: string
   age: number
 }
+
+export interface IRootWidthModule {
+  login: ILoginState
+}
+
+export type IStoreType = IRootState & IRootWidthModule
