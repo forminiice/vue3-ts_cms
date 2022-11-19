bcb8754e9c39e09307cec58c3353b58a5538ee56
Hz <forminiice@qq.com>
2022-11-19 21:30:39 +0800

    feat: add Dynamic Routing page

---
+++ src/router/main/analysis/bashboard/dashboard.ts
@@ -0,0 +1,7 @@
+const dashboard = () => import('@/views/main/analysis/bashboard/dashboard.vue')
+export default {
+  path: '/main/analysis/bashboard',
+  name: 'dashboard',
+  component: dashboard,
+  children: []
+}
